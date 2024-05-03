3d31f19cb7027a1e6b4c466f4f9cd44dd24ac428
karinak2001 <karinak2001@edu.aac.ac.il>
2024-05-03 22:13:27 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your fav color? ");
+        String s = scanner.next();   // קולט את המילה לתוך משתנה מחרוזת
+
+        System.out.println(s);
+
+
+
+
+    }
+}
